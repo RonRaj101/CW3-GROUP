88ae14a68731697f2b1209762e9825fb07cb3819
Ronit Rai <43289911+RonRaj101@users.noreply.github.com>
2023-02-28 17:21:59 +0000

    sw.js

--- sw.js
+++ sw.js
@@ -14,3 +14,18 @@ return cache.addAll(cacheFiles);
 });
 
 self.addEventListener('fetch',() => console.log("fetch"));
+
+self.addEventListener('fetch', function (e) {
+    e.respondWith(
+        // check if the cache has the file        
+        caches.match(e.request).then(function (r) {
+            // download file if not in the cache
+            return r || fetch(e.request).then(function(response){
+                // add the new file to cache
+                return caches.open(cacheName).then(function(cache){
+                    cache.put(e.request, response.clone());
+                    return response;
+                })
+            })
+        })
+)});
